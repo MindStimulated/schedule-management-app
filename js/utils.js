f01bb7126a9974e6d7d0fc9c899905968e245cd6
MindStimulated <mindstimulatedmarketing@gmail.com>
2025-06-04 16:49:57 -0700

    Create utils.js

---
+++ js/utils.js
@@ -0,0 +1,18 @@
+// Utility functions
+function calculateHours(timeString) {
+    // Time calculation logic
+}
+
+function parseTime(timeStr) {
+    // Time parsing logic
+}
+
+function showToast(message, type) {
+    // Toast notification
+}
+
+function showLoading(show) {
+    // Loading indicator
+}
+
+// Other utility functions...
