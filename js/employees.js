675be6dfeeea87f0da425d1b9291a947076261eb
MindStimulated <mindstimulatedmarketing@gmail.com>
2025-06-04 16:49:13 -0700

    Create employees.js

---
+++ js/employees.js
@@ -0,0 +1,18 @@
+// Employee management functions
+function openEmployeeModal() {
+    // Open modal for adding/editing employees
+}
+
+function saveEmployee(event) {
+    // Save employee data
+}
+
+function editEmployee(employeeName) {
+    // Edit existing employee
+}
+
+function deleteEmployee(employeeName) {
+    // Delete employee
+}
+
+// Other employee-related functions...
