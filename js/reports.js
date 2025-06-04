f828d796848b457be9403f3931be62a87db4831c
MindStimulated <mindstimulatedmarketing@gmail.com>
2025-06-04 16:49:40 -0700

    Create reports.js

---
+++ js/reports.js
@@ -0,0 +1,18 @@
+// Shift report functions
+function openNewReportModal() {
+    // Open modal for new report
+}
+
+function saveShiftReport(event) {
+    // Save report data
+}
+
+function viewReports() {
+    // View all reports
+}
+
+function viewReport(reportId) {
+    // View specific report
+}
+
+// Other report-related functions...
